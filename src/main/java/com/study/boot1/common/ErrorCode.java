b60bdcfd13a007f56c0e4fd73b781555e822d0bf
Seong Hyeok Lee <dltjdgur327@gmail.com>
2019-01-26 23:08:20 +0900

    ErrorCode HttpStatus 수정

--- src/main/java/com/study/boot1/common/ErrorCode.java
+++ src/main/java/com/study/boot1/common/ErrorCode.java
@@ -1,22 +1,30 @@
 package com.study.boot1.common;
 
+import org.springframework.http.HttpStatus;
+
 public enum  ErrorCode {
 
-    INVALID_PARAM_ACCOUNT_TYPE(10001, "INVALID_PARAM_ACOUNT_TYPE"),
+    INVALID_PARAM_ACCOUNT_TYPE(HttpStatus.BAD_REQUEST, 10001, "INVALID_PARAM_ACOUNT_TYPE"),
 
-    EXCEED_POST_LIST_MAX_COUNT(20001, "EXCEED_POST_LIST_MAX_COUNT"),
+    EXCEED_POST_LIST_MAX_COUNT(HttpStatus.BAD_REQUEST, 20001, "EXCEED_POST_LIST_MAX_COUNT"),
 
-    INVALID_QUERY_EXCEPTION(30001, "INVALID_QUERY_EXCEPTION"),
+    INVALID_QUERY_EXCEPTION(HttpStatus.BAD_REQUEST, 30001, "INVALID_QUERY_EXCEPTION"),
 
-    UNKNOWN(50000, "UNKNOWN")
+    UNKNOWN(HttpStatus.BAD_REQUEST, 50000, "UNKNOWN")
     ;
 
+    private final HttpStatus httpStatus;
     private final int code;
     private final String msg;
 
-    ErrorCode (int code, String msg){
+    ErrorCode (HttpStatus httpStatus, int code, String msg){
+        this.httpStatus = httpStatus;
         this.code = code;
         this.msg = msg;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
     }
 
     public int getCode() {
